d26416d65166d3fe0beadf5100748e1100e76d69
joseph <leeyosebi@gmail.com>
2023-11-29 13:47:11 +0900

    changed2

---
+++ UnifiedGroup.ps1
@@ -0,0 +1,17 @@
+﻿Connect-ExchangeOnline
+Get-UnifiedGroup -Identity '[GTR] M365 Help Desk' | Set-UnifiedGroup -UnifiedGroupWelcomeMessageEnabled: $false
+Get-UnifiedGroup -Identity '[GTR] M365 Help Desk' | Set-UnifiedGroup -HiddenFromExchangeClientsEnabled: $true
+
+Get-UnifiedGroup -Identity '[GTR] M365 Help Desk' | select *
+
+
+Get-UnifiedGroup | Where-Object {$_.displayname -like "*Desk"}
+
+[GEU/EHQ] M365 Help Desk
+[GSK] M365 Help Desk
+[GCZ] M365 Help Desk
+[GTR] M365 Help Desk
+
+Disconnect-ExchangeOnline
+
+Get-UnifiedGroup | Where-Object {$_.displayname -like "* help desk"}
