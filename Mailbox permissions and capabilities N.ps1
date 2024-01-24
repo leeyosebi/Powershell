43eedf78bf1dc94c1f714f1e9ddeaafcc4e9f80c
joseph <leeyosebi@gmail.com>
2024-01-24 16:54:45 +0900

    exchange hyvrid sendAS Permission2

--- Mailbox permissions and capabilities N.ps1
+++ Mailbox permissions and capabilities N.ps1
@@ -11,7 +11,7 @@
         $TargetADUser = $ADUsers | where {$_.UserprincipalName -eq user@domain.name}
         Add-ADPermission -Identity $TargetDL.Identity -User $TargetADUser.UserPrincipalName -AccessRights ExtendedRight -ExtendedRights "Send As"
 
-    #EXO
+        #EXO
         $TargetDL = Get-DistributionGroup -Identity A@domain.name
         $TargetAADUser = $AADUsers | where {$_.UserprincipalName -eq user@domain.name}
         Add-RecipientPermission -Identity $TargetDL.Identity -Trustee $TargetAADUser.UserPrincipalName -AccessRights SendAs
