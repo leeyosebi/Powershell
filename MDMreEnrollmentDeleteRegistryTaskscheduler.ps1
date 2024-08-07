1a4d182ad9cb4c28bd6ad422067f2abc7777206e
joseph <leeyosebi@gmail.com>
2024-08-07 15:54:47 +0900

    End User PC에서 MDM 자동 등록을 위해 기존 구성을 삭제하는 파워쉘

--- MDMreEnrollmentDeleteRegistryTaskscheduler.ps1
+++ MDMreEnrollmentDeleteRegistryTaskscheduler.ps1
@@ -1,12 +1,32 @@
-﻿Connect-ExchangeOnline
-Get-UnifiedGroup -Identity 'M365 Help Desk' | Set-UnifiedGroup -UnifiedGroupWelcomeMessageEnabled: $false
-Get-UnifiedGroup -Identity 'M365 Help Desk' | Set-UnifiedGroup -HiddenFromExchangeClientsEnabled: $true
+﻿Get-ChildItem 'Cert:\LocalMachine\My\' | ? Issuer -EQ "CN=Microsoft Intune MDM Device CA" | % {
+    Remove-Item $_.PSPath
+}
+ 
+$EnrollmentGUIDAll = Get-ScheduledTask | Where-Object { $_.TaskPath -like "*Microsoft*Windows*EnterpriseMgmt\*" } | Select-Object -ExpandProperty TaskPath -Unique | Where-Object { $_ -like "*-*-*" } | Split-Path -Leaf
+ 
+foreach ($EnrollmentGUID in $EnrollmentGUIDAll)
+{
+    $RegistryKeys = "HKLM:\SOFTWARE\Microsoft\Enrollments", "HKLM:\SOFTWARE\Microsoft\Enrollments\Status", "HKLM:\SOFTWARE\Microsoft\EnterpriseResourceManager\Tracked", "HKLM:\SOFTWARE\Microsoft\PolicyManager\AdmxInstalled", "HKLM:\SOFTWARE\Microsoft\PolicyManager\Providers", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Accounts", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Logger", "HKLM:\SOFTWARE\Microsoft\Provisioning\OMADM\Sessions"
+    foreach ($Key in $RegistryKeys) {
+        if (Test-Path -Path $Key) {
+            Get-ChildItem -Path $Key | Where-Object { $_.Name -match $EnrollmentGUID } | Remove-Item -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
+        }
+    }
+   
+    Get-ScheduledTask | Where-Object { $_.Taskpath -match $EnrollmentGUID } | Unregister-ScheduledTask -Confirm:$false
+   
+    Remove-Item -Path "$env:WINDIR\System32\Tasks\Microsoft\Windows\EnterpriseMgmt\$EnrollmentGUID" -Force
+   
+    $scheduleObject = New-Object -ComObject Schedule.Service
+    $scheduleObject.connect()
+    $rootFolder = $scheduleObject.GetFolder("\Microsoft\Windows\EnterpriseMgmt")
+    $rootFolder.DeleteFolder($EnrollmentGUID,$null)
+   
+   
+}
+ 
+#MDM Autoenrollment GPO 를 할당해서 스케쥴잡을 생성하는 경우, 커멘트 기호 해제
+#gpupdate /force
 
-Get-UnifiedGroup -Identity 'M365 Help Desk' | select *
-
-
-Get-UnifiedGroup | Where-Object {$_.displayname -like "*Desk"}
-
-Disconnect-ExchangeOnline
-
-Get-UnifiedGroup | Where-Object {$_.displayname -like "* help desk"}
+#MDM Autoenrollment GPO 를 할당하지 않고, 강제로 Intune Enrollment 실행하는 경우, 커멘트 기호 해제
+#$env:WINDIR\System32\DeviceEnroller.exe /C /AutoenrollMDM
