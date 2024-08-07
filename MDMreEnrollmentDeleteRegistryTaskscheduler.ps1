c32f7726be8c99a96c0c2587aaebde5928fb61e7
joseph <leeyosebi@gmail.com>
2024-08-07 15:46:31 +0900

    End User PC에서 MDM 자동 등록을 위해 기존 구성을 삭제하는 파워쉘

---
+++ MDMreEnrollmentDeleteRegistryTaskscheduler.ps1
@@ -0,0 +1,12 @@
+﻿Connect-ExchangeOnline
+Get-UnifiedGroup -Identity 'M365 Help Desk' | Set-UnifiedGroup -UnifiedGroupWelcomeMessageEnabled: $false
+Get-UnifiedGroup -Identity 'M365 Help Desk' | Set-UnifiedGroup -HiddenFromExchangeClientsEnabled: $true
+
+Get-UnifiedGroup -Identity 'M365 Help Desk' | select *
+
+
+Get-UnifiedGroup | Where-Object {$_.displayname -like "*Desk"}
+
+Disconnect-ExchangeOnline
+
+Get-UnifiedGroup | Where-Object {$_.displayname -like "* help desk"}
