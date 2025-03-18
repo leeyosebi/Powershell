78c10498aba26cad6733e4a0849eb03d0c71552d
joseph <leeyosebi@gmail.com>
2025-03-18 15:53:33 +0900

    Manual Hybrid Setup with Powershell edited_5st

--- ExchangeServer/Exchange Hybrid with Edge Server.ps1
+++ ExchangeServer/Exchange Hybrid with Edge Server.ps1
@@ -23,7 +23,7 @@ Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
     #. 3 Connectors
         New-SendConnector -Name 'Outbound to Office 365 - b5979a4d-3184-4d53-add6-0db08a57fe7d' -AddressSpaces 'smtp:M365x67099404.mail.onmicrosoft.com;1' -DNSRoutingEnabled: $true -ErrorPolicies Default -Fqdn 'jose.run.place' -RequireTLS: $true -IgnoreSTARTTLS: $false -SourceTransportServers JOSEEDGE01 -SmartHosts $null -TLSAuthLevel DomainValidation -DomainSecureEnabled: $false -TLSDomain 'mail.protection.outlook.com' -CloudServicesMailEnabled: $true -TLSCertificateName '<I>CN=ZeroSSL RSA Domain Secure Site CA, O=ZeroSSL, C=AT<S>CN=jose.run.place'
         Set-PartnerApplication -Identity 'Exchange Online' -Enabled: $true
-
+                
 
 <#------------------------------------------- Phase 2: Exchange Online-------------------------------------------#>
 
@@ -34,3 +34,12 @@ Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
         Test-MigrationServerAvailability -ExchangeRemoteMove: $true -RemoteServer 'mail.jose.run.place' -Credentials (Get-Credential -UserName JOSE\joseadmin)
         New-MigrationEndpoint -Name 'Hybrid Migration Endpoint - EWS (Default Web Site)' -ExchangeRemoteMove: $true -RemoteServer 'mail.jose.run.place' -Credentials (Get-Credential -UserName JOSE\joseadmin)
         Set-OnPremisesOrganization -Identity '28bb35b7-47ef-4ffb-8a58-33cdf00dade3' -Comment 'rZTbTttAEIbnUaxe0YvQHByTIBoJTBBUHKQamuvEa2iAxMhJWnj5lm/GDoraBMdVZa135/j/Ozu7v38dSCgLyfgSmcpcjiWViQxljOTJtbzIE5bP8kEi1jM8Euy7SHNi1OsOW0++yBW6PpavciOXzOdoQjlkPpBP76D0sF8gO2xVEE/I+Mh3itfILM6Qlrk07xWWe9YxsSH6qdzidwdKBvqU7LdYs3dxj4hLwUksIkceIs/QKN62GMonJG5c1OCbaWfIaUmtz8w/EN/QfdmTjrSlzty1f8DckBajLc2i2ptwlMUxvDT3NboJ8w1SbFIsD3huzyUAtQMDZVTnC1gpo7axbMPINz5liMoqMp4/7P/vtWnap9VoWUWUn7IMjKdy2YyTd6KeXWZ8c22f4UBPrffmILqismPGS6WeXe1Vbw2W9xeaJzXTDsjlyQ6VTOinofW+WgfII+bI2CTysbgDVXeRd6d6ToteTok/JOLJeileyXXJelLxtrboixFn4Iy9Y1ddcjpOrMbwsXRZjTixDvqa3Zk6ni070z1kZzvPu7saT93bOevZ2w1djQ3lu1UlLtlR2TuwHcL6Op9gzSy6L8+GnxX3N/4v9zOwl6LJ2qfCdeYGmo69G+srWo2R7mpgcQ+c0SOrn3Tq4o/e3sx6+S5E2JfV1fdkn9Hgr12xy3vStfel8faulGGu4xXhE9suEuspV/HU9V1fFGdenrsnrw=='
+
+
+<#------------------------------------------- Phase 3: Edge Server - After HCW-------------------------------------------#>
+
+    # 0. HCW8056 - Receive connector on the following Edge Transport servers need to be manually configured: JOSEEDGE01
+        Set-ReceiveConnector -Identity "JOSEEDGE01\Default internal receive connector JOSEEDGE01" -TlsDomainCapabilities mail.protection.outlook.com:AcceptOorgProtocol -Fqdn 'jose.run.place'
+    
+    # 1. Certificate
+        # Import Public Certificate of jose.run.place and assign SMTP Services. Lastly, execute iisreset.
