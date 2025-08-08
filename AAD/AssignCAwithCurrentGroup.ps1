1bd982144323a057d7cef8b7e8bb06ff4ccca19f
joseph <leeyosebi@gmail.com>
2025-08-08 14:22:37 +0900

    CA

---
+++ AAD/AssignCAwithCurrentGroup.ps1
@@ -0,0 +1,30 @@
+# MgGraph login
+Connect-MgGraph -Scopes "Policy.Read.All", "Policy.ReadWrite.ConditionalAccess", "DeviceManagementConfiguration.ReadWrite.All"
+
+# Identify Group ID:
+# Get-MgGroup -Filter "DisplayName eq 'Group Name'"
+
+$GroupId = "Group ID"
+$newGroupId = "New Group ID"
+
+# Identify the policies
+$policies = Get-MgIdentityConditionalAccessPolicy -All | Where-Object {
+    $_.Conditions.Users.IncludeGroups -contains $GroupId -or
+    $_.Conditions.Users.ExcludeGroups -contains $oldGroupId
+}
+
+foreach ($p in $policies) {
+    # if Group in IncludeGroups + NewGroup in IncludeGroups
+    if ($p.Conditions.Users.IncludeGroups -contains $GroupId) {
+        $p.Conditions.Users.IncludeGroups += $newGroupId
+    }
+    # if Group in ExcludeGroups + NewGroup in ExcludeGroups
+    if ($p.Conditions.Users.ExcludeGroups -contains $GroupId) {
+        $p.Conditions.Users.ExcludeGroups += $newGroupId
+    }
+
+    # Update
+    Update-MgIdentityConditionalAccessPolicy -ConditionalAccessPolicyId $p.Id -BodyParameter @{
+        Conditions = $p.Conditions
+    }
+}
